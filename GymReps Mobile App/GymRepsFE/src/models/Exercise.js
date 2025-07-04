46165b8ee97d459939587a81d45b64b46b1d94e3
vincedenisa <115655901+vincedenisa@users.noreply.github.com>
2025-07-04 21:04:35 +0300

    Add files via upload

---
+++ GymReps Mobile App/GymRepsFE/src/models/Exercise.js
@@ -0,0 +1,9 @@
+export class Execise {
+    constructor(id, name, sets, reps, restTime) {
+        this.id = id;
+        this.name = name;
+        this.sets = sets;
+        this.reps = reps;
+        this.restTime = restTime;
+    }
+}
